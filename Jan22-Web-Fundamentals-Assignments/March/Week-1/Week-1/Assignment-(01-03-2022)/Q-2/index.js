de910d921531b3f0beda6dc66fb73a5d3144e602
pawan kumar sharma <pawan1995219@gmail.com>
2022-03-13 16:51:50 +0530

    Delete Jan22-Web-Fundamentals-Assignments/March/Week-1 directory

--- Jan22-Web-Fundamentals-Assignments/March/Week-1/Week-1/Assignment-(01-03-2022)/Q-2/index.js
+++
@@ -1,7 +0,0 @@
-function add() {
-    var a=Number(document.getElementById("num1").value);
-    var b=Number(document.getElementById("num2").value);
-    c=a+b;
-    document.getElementById("sum").value=c;
-    
-}

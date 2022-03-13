cd642f552049ff858b44bafb363f8b09ad925f51
pawan kumar sharma <pawan1995219@gmail.com>
2022-03-13 16:45:56 +0530

    Add files via upload

---
+++ Jan22-Web-Fundamentals-Assignments/March/Week-1/Week-2/07-03-2022/Q-2/index.js
@@ -0,0 +1,8 @@
+function toggle(){
+	var disp = document.getElementsByClassName('profile')[0]
+     if (disp.style.display == 'block') {
+     	disp.style.display = 'none'
+     }else{
+     	disp.style.display = 'block'
+     }
+}
